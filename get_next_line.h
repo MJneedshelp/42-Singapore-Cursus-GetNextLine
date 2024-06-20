85d58a258f88b7d705b16993a1a16c5564cf04de
MJ 42 <mingjuntan93@gmail.com>
2024-06-20 20:33:50 +0800

    added header file and function prototype

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mintan <mintan@student.42singapore.sg      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 17:23:17 by mintan            #+#    #+#             */
+/*   Updated: 2024/06/20 20:30:34 by mintan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+
+
+#endif
--- testread.c
+++ testread.c
@@ -6,7 +6,7 @@
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 19:33:08 by mintan            #+#    #+#             */
-/*   Updated: 2024/06/19 19:33:08 by mintan           ###   ########.fr       */
+/*   Updated: 2024/06/20 20:29:05 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	main(void)
 	size_t	buffsz;
 	char	*buff;
 
-	fd = open("test.txt", O_RDONLY);
+	fd = open("testtext", O_RDONLY);
 	printf("fd value: %d\n", fd);
 	if (fd == -1)
 	{
@@ -31,20 +31,20 @@ int	main(void)
 		return (-1);
 	}
 
+/*	
 	readsz = 10;
 	buffsz = 10;
 	buff = (char *)malloc(buffsz * sizeof(char));
-	while(readsz != 0)
+	while (readsz != 0)
 	{
-		readsz = read(fd, buff, buffsz);
-		buff[buffsz] = '\0';
+		readsz = read(fd, buff, buffsz - 1);
+		buff[readsz] = '\0';
 		printf("Read size: %d\n", readsz);
 		printf("Read content: %s\n", buff);
 	}
-	//Do smth here
-	//Do smth here
-	//Do smth here
-	//Do smth here
+*/
+
+// Get_next_line here
 
 	closechk = close(fd);
 	printf("close value: %d\n", closechk);
@@ -54,5 +54,4 @@ int	main(void)
 		return (-1);
 	}
 	return (0);
-
 }
