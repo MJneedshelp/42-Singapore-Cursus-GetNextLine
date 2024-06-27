ae9bc5cd49facda654538b5e96fccdbd2f983c2f
MJ <mingjuntan93@gmail.com>
2024-06-27 10:11:30 +0800

    Updated GNL logic for readsz == 0 and readsz == -1

--- get_next_line draft2.c
+++ get_next_line draft2.c
@@ -36,11 +36,8 @@ size_t	findn(char *str)
 /* Description: Frees the remainder and sets it back to NULL. */
 void	freerem(char **rem)
 {
-	if (rem)
-	{
-		free (*rem);
-		*rem = NULL;
-	}
+	free (*rem);
+	*rem = NULL;
 }
 
 /* Description: Takes in the remainder and checks remainder for \n. Returns
@@ -88,6 +85,8 @@ void	freeptr(void *ptr1, void *ptr2, void *ptr3)
 		free (ptr3);
 }
 
+
+
 /* Description: Checks if a file can be read
    //probably smth to check if the FD passed in is a binary file or smth
 */
@@ -109,59 +108,41 @@ char	*get_next_line(int fd)
 	int			readsz;
 	int			stat;
 
-/******************************************************************************/
-	//1. function to check if the FD is readable
-/******************************************************************************/
-
 	if (rem)
 	{
 		retstr = remcheck(&rem, &stat);
 		if (retstr != NULL || stat == -1)
 			return (retstr);
 	}
-
-//Probably need to split the next read part out as a function
-	//Allocate buffer at the start to store the items to be read
-	//Buffer size + 1
 	readsz = BUFFER_SIZE;
 	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-
-	//keep reading the file while not at the end and while there are no errors
-	//readsz > 0. Check if readsz == -1 (to include this)
 	while (readsz > 0)
 	{
 		readsz = read(fd, buff, BUFFER_SIZE);
 		buff[readsz] = '\0';
-		//printf("Current rem: %s | Read size: %d | Buff: %s\n", rem, readsz, buff);
-		//If new content is read into the buffer
 		if (readsz > 0)
 		{
-			//Check if there is a \n in the buffer
 			npos = findn(buff);
 			if (npos >= 0)
 			{
-				//Retrieve the substring up to \n in the buffer -> linen
 				linen = ft_substr((const char *)buff, 0, npos + 1);
 				if (linen == NULL)
 				{
-					freerem(&rem);
+					if (rem)
+						free(rem);
 					return (free(buff), NULL);
 				}
-				//Check if there's remainder from previous run. Concat rem and linen -> retstr
 				if (rem)
 				{
 					retstr = ft_strjoin(rem, linen);
 					if (retstr == NULL)
 						return (free(buff), free(rem), free(linen), NULL);
-					//Free rem and linen after generating retstr
-					freerem(&rem);
+					free (rem);
+					rem = NULL;
 					free (linen);
 				}
-				//No remainder from previous run. linen -> retstr
 				else
 					retstr = linen;
-				//if there is extra content behind the \n in buff, set that content as the new remainder
-				//the old remainder should have been destroyed by this time
 				if ((npos + 1) < readsz)
 				{
 					rem = ft_substr(buff, npos + 1, readsz - (npos + 1));
@@ -169,34 +150,33 @@ char	*get_next_line(int fd)
 						return (free(buff), free(retstr), NULL);
 				}
 				free (buff);
-				//Return for \n is found in new read buff
 				return (retstr);
 			}
-			//No \n found in the buff
 			else
 			{
-				//if rem != NULL, concat rem and buff -> temporary rem
-				//Free old rem first and assgin tmprem as new rem
 				if (rem)
 				{
 					tmprem = ft_strjoin(rem, buff);
 					if (tmprem == NULL)
 						return (free(rem), free(buff), NULL);
-					free(rem);
+					free (rem);
 					rem = tmprem;
 				}
-				//No old rem. Duplicate buff as the new rem. buff is kept to read more in the next loop
 				else
+				{
 					rem = ft_strdup(buff);
+				}
 			}
 		}
+		else if (readsz == -1)
+		{
+			free (buff);
+			if (rem != NULL)
+				free (rem);
+			return (NULL);
+		}
 	}
-	//Readsz == 0, finished reading the file. Free buff and return rem if there is anything left in there. If no rem, return
-	//Or readsz == -1, failed read. Free buff. Free rem (if there is smth). Return NULL
-	free (buff);
-	if (readsz == -1)
-		freerem(&rem);
-	else if (readsz == 0 && rem != NULL)
+	if (rem != NULL)
 		return (rem);
 	return (NULL);
 }
